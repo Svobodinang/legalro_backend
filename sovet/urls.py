d6cbb30fbdda909cc913ee0c2a008d0793fccac5
Nadezhda <svobodinang@gmail.com>
2020-10-08 23:32:02 +0300

    get services

--- sovet/urls.py
+++ sovet/urls.py
@@ -9,7 +9,7 @@ router.register(r'Goals', GoalsViewSet)
 router.register(r'Docs', DocsViewSet)
 router.register(r'ServiceBlock', ServiceBlockViewSet)
 router.register(r'ServiceSection', ServiceSectionViewSet)
-router.register(r'Service', ServiceSectionViewSet)
+router.register(r'Service', ServiceViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
